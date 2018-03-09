19c88ce7b846c773cf87b599d347052a841fd0b9
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-09 08:28:24 -0500

    Updated from Brython Server: 3/9/2018 8:28:23 AM

--- functionDemo.py
+++ functionDemo.py
@@ -3,5 +3,23 @@
 #functionDemo.py
 
 def hw():
-    print("hello, world')
+    print("hello, world")
 
+def double(thingToDouble):
+    print(thingToDouble*2)
+
+def bigger(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+
+#bigger(4,3)
+#bigger(3,4)
+#bigger('Ray','Tso')
+#bigger(True,False)
+
+def slope(x1,y1,x2,y2):
+    print((y2-y1)/(x2-x1))
+
+slope(1,1,2,2)
