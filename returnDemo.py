02184ce07a6ca2193c1acf392c3d7b9294d23950
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-21 18:47:02 -0400

    Update returnDemo.py

--- returnDemo.py
+++ returnDemo.py
@@ -5,15 +5,14 @@
 from random import randint
 
 def randEven(low,high):
-    n=randint(low,high)
-    while n%2!=0:
-        n=randint(low,high)
+    n = randint(low,high)
+    while n%2 != 0:
+        n = randint(low,high)
     return n
-
+    
 print("your even numbers are",randEven(0,100),randEven(0,100),randEven(0,100))
-
-if num4< 1500:
+num4 = randEven(1000,2000)
+if num4 < 1500:
     print(num4, "is pretty big")
-
 else:
-    print(num4, " is really big")
+    print(num4, "is really big")
