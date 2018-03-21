e7825aa4d4b89e513ec180cf963954cde09a4599
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-21 19:09:24 -0400

    Updated from Brython Server: 3/21/2018 7:09:23 PM

--- coloChangeWindow .py
+++ coloChangeWindow .py
@@ -1,3 +1,24 @@
 #Ray Tso
 #3/19/18
 #rcolorChangeWindow.py
+from random import randint
+from g game import*
+def mouseClick(event):
+
+red=Color(0xFF0000,1) #this is the color red
+green=Color(0x00FF00,1)
+blue=Color(0x0000FF,1)
+black=Color(0x000000,1)
+randint(1,4)
+
+if num==1:
+    color= red
+
+elif num==2:
+    color=green
+    
+else num==3:
+    color=blue
+
+else:
+    color=black
