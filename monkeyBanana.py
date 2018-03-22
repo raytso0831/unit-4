a15255d40661fbe4cc92e6877916fbf2c96fc2b9
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-22 09:02:10 -0400

    Updated from Brython Server: 3/22/2018 9:02:09 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -6,74 +6,67 @@ from ggame import *
 from random import randint
 
 #constants
-ROWS=20
-COLS=40
-CELL_SIZE=20
+ROWS = 22
+COLS = 44
+CELL_SIZE = 20
 
 def moveRight(event):
-    monkey.x+=CELL_SIZE
-    if monkey.x==banana.x and monkey.y==banana.y:
-        moveBanana()
-        data['score']+=10
-        print(data['score'])
-
-
+    if monkey.x < (COLS-1)*CELL_SIZE:
+        monkey.x += CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            data['score'] += 10
+            print(data['score'])
+    
 def moveLeft(event):
-    if monkey.x>0:
-        monkey.x-=CELL_SIZE
-        if monkey.x==banana.x and monkey.y==banana.y:
-             moveBanana()
-             data['score']+=10
-             print(data['score'])
-
+    if monkey.x > 0:
+        monkey.x -= CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            data['score'] += 10
+            print(data['score'])
     
 def moveUp(event):
-        if monkey.x>0:
-            monkey.y-=CELL_SIZE
-            if monkey.x==banana.x and monkey.y==banana.y:
-             moveBanana()
-             data['score']+=10
+    if monkey.y > 0:
+        monkey.y -= CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            data['score'] += 10
             print(data['score'])
-
-
-def moveDown(event):
-    monkey.y+=CELL_SIZE
-    if monkey.x==banana.x and monkey.y==banana.y:
-             moveBanana()
-             data['score']+=10
-             print(data['score'])
-
     
+def moveDown(event):
+    if monkey.y < (ROWS-1)*CELL_SIZE:
+        monkey.y += CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            data['score'] += 10
+            print(data['score'])
     
 def moveBanana():
-    banana.x=randint(0,COLS-1)*CELL_SIZE
-    banana.y=randint(0,COLS-1)*CELL_SIZE
+    banana.x = randint(0,COLS-1)*CELL_SIZE
+    banana.y = randint(0,ROWS-1)*CELL_SIZE
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     
     #hold variables in a dictionary
-    data={}
-    data['score']=0
-    
+    data = {}
+    data['score'] = 0
     
     #colors
-    green=Color(0x006600,1)
-    brown=Color(0x8B4513,1)
-    yellow=Color(0XFFFF00,1)
+    green = Color(0x006600,1)
+    brown = Color(0x8B4513,1)
+    yellow = Color(0xFFFF00,1)
     
-    jungleBox=RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
-    monkeyBox=RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
-    bananaBox=RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow),yellow)
+    jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
+    monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
+    bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow),yellow)
     
     Sprite(jungleBox)
-    monkey=Sprite(monkeyBox)
-    banana=Sprite(bananaBox,(CELL_SIZE*COLS/2, CELL_SIZE*ROWS/2))
+    monkey = Sprite(monkeyBox)
+    banana = Sprite(bananaBox,(CELL_SIZE*COLS/2,CELL_SIZE*ROWS/2))
     
-    
-    App().listenKeyEvent('keydown','right arrow', moveRight)
-    App().listenKeyEvent('keydown','left arrow', moveLeft)
-    App().listenKeyEvent('keydown','up arrow', moveUp)
-    App().listenKeyEvent('keydown','down arrow', moveDown)
-
+    App().listenKeyEvent('keydown','right arrow',moveRight)
+    App().listenKeyEvent('keydown','left arrow',moveLeft)
+    App().listenKeyEvent('keydown','up arrow',moveUp)
+    App().listenKeyEvent('keydown','down arrow',moveDown)
     App().run()
