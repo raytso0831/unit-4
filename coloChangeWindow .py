f0a699667de9d0bd5764e06bdba5c95fbe7e1a05
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-22 07:58:41 -0400

    Updated from Brython Server: 3/22/2018 7:58:40 AM

--- coloChangeWindow .py
+++ coloChangeWindow .py
@@ -1,15 +1,15 @@
 #Ray Tso
 #3/19/18
-#rcolorChangeWindow.py
+#colorChangeWindow.py
 from random import randint
 from g game import*
 def mouseClick(event):
-
-red=Color(0xFF0000,1) #this is the color red
-green=Color(0x00FF00,1)
-blue=Color(0x0000FF,1)
-black=Color(0x000000,1)
-randint(1,4)
+    
+    red=Color(0xFF0000,1) #this is the color red
+    green=Color(0x00FF00,1)
+    blue=Color(0x0000FF,1)
+    black=Color(0x000000,1)
+    num=randint(1,4)
 
 if num==1:
     color= red
@@ -17,8 +17,11 @@ if num==1:
 elif num==2:
     color=green
     
-else num==3:
+elif num==3:
     color=blue
 
 else:
     color=black
+
+Outline=LineStyle(1, color)
+Square=SquareAsset(1000,1000,Outline,color) #width, height, outline, fill
