3b40b332e801d148183d742e1d7b39a5f871ec73
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-23 08:49:32 -0400

    Updated from Brython Server: 3/23/2018 8:49:31 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -10,20 +10,23 @@ ROWS = 22
 COLS = 44
 CELL_SIZE = 20
 
+#moves monkey right cell if possible
 def moveRight(event):
     if monkey.x < (COLS-1)*CELL_SIZE:
         monkey.x += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-    
+
+#moves monkey left cell if possible
 def moveLeft(event):
     if monkey.x > 0:
         monkey.x -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-    
+            
+#moves monkey up cell if possible
 def moveUp(event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
@@ -31,6 +34,7 @@ def moveUp(event):
             moveBanana()
             updateScore()
     
+#moves monkey down cell if possible
 def moveDown(event):
     if monkey.y < (ROWS-1)*CELL_SIZE:
         monkey.y += CELL_SIZE
@@ -38,28 +42,40 @@ def moveDown(event):
             moveBanana()
             updateScore()
            
-    
+#increase score and display new text at bottom of screen    
 def updateScore():
     data['score'] += 10
-    data['scoreText'].destroy()
+    data['scoreText'].destroy()#remove old writing
     scoreBox=TextAsset('Score='+str(data['score']))
     data['scoreText']=Sprite(scoreBox,(0,ROWS*CELL_SIZE))
 
+#move banana to a random location and reset timer
 def moveBanana():
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
+
+#keep track of many frames have happened and moves banana if it
+#has been more than 300 frames since last time
+
+def step():
+    data['frames']+=1
+    if data['frames']%200 ==0:
+        moveBanana()
 
 if __name__ == '__main__':
     
     #hold variables in a dictionary
     data = {}
     data['score'] = 0
+    data['frames']=0
+   
     
     #colors
     green = Color(0x006600,1)
     brown = Color(0x8B4513,1)
     yellow = Color(0xFFFF00,1)
     
+    #graphics
     jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,yellow),yellow)
@@ -76,4 +92,4 @@ if __name__ == '__main__':
     App().listenKeyEvent('keydown','left arrow',moveLeft)
     App().listenKeyEvent('keydown','up arrow',moveUp)
     App().listenKeyEvent('keydown','down arrow',moveDown)
-    App().run()
+    App().run(step)
