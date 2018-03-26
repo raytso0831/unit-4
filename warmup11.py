79e589549cb1ded1f0fdab9b1ed08d31f444c10b
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-26 08:28:13 -0400

    Updated from Brython Server: 3/26/2018 8:28:12 AM

--- warmup11.py
+++ warmup11.py
@@ -2,5 +2,16 @@
 #3/26/18
 #warmup11.py
 
+def prime (n):
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+    
+
+print(prime(9))
+print(prime(10))
+print(prime(11))
+
 
 
