5766537f8ab480f90062302ff956aaff116c5197
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-28 08:36:01 -0400

    Updated from Brython Server: 3/28/2018 8:36:01 AM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,21 @@
+#Ray Tso
+#3/28/18
+#stringUnion.py
 
+def stringUnion(word1,word2):
+    totalStr = ''
+    totalStr += word1
+        
+    totalStr +=word2
+    finalStr = ''
+    for ch in totalStr:
+        if ch in word1:
+            if not ch in finalStr:
+                finalStr += ch
+        if ch in word2:
+            if not ch in finalStr:
+                finalStr+= ch
+    return finalStr
+    
+print(stringUnion('mississippi','pennsylvania'))
+    
