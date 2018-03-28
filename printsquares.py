ad64bb958ea3d339645e83607fef7760b844ffdf
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-03-28 08:17:14 -0400

    Updated from Brython Server: 3/28/2018 8:17:13 AM

--- printsquares.py
+++ printsquares.py
@@ -1,1 +1,11 @@
+#Ray Tso
+#3/28/18
+#printsquare.py
 
+def printSquares(num1,num2):
+    for i in range(1,num1+1):
+        print('+--'*num2+'+')
+        print('|  '*num2+'|')
+    print('+--'*num2+'+')
+    
+printSquares(2,4)
