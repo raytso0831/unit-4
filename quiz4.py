51b5241e019f1b18eac82f295956ce74b146a3eb
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-04-02 08:37:27 -0400

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,31 @@
+#Ray Tso
+#4/2/18
+#quiz.py
 
+def count(num1):
+    for i in range(1,num1+1):
+        print(i)
+
+count(7)
+
+def excitedPrint(string):
+    print(string.upper(), '!!!')
+
+excitedPrint("i love <3 comp programming")
+
+def firstLetter(word):
+    for i in word:
+        return(i)
+        
+print(firstLetter("Ray"))
+
+def repeats(num1,num2,num3):
+    if num1 == num2 or num2 == num3 or num1 == num3:
+        return True
+    else:
+        return False
+
+
+
+print(repeats(3,4,5))
+print(repeats(1,0,1))
